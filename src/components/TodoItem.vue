42f39d80010e5ea877fc519f7aaf9045db2e0fcb
KaranP3 <karanpillai93@gmail.com>
2020-03-09 02:36:08 +0530

    Basic UI with toggle and delete in state
    Changes made to index.html, App.vue, TodoInput.vue, TodoItem.vue, and Todos.vue

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <li v-bind:class="{'completed': todo.completed}">
+    <div class="form-check">
+      <label class="form-check-label">
+        <input class="checkbox" type="checkbox" :checked="todo.completed" v-on:change="markComplete"/>
+        {{todo.title}}
+        <i class="input-helper"></i>
+      </label>
+    </div>
+    <i @click="$emit('del-todo', todo.id)" class="remove icon-trash" style="font-size: smaller;"></i>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"], 
+  methods: {
+      markComplete(){
+          this.todo.completed = !this.todo.completed
+      }
+  }
+};
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="row container d-flex justify-content-center">
+      <div class="col-lg-12">
+        <div class="card px-3 z-depth-1">
+          <div class="card-body">
+            <h4 class="card-title">vue-todos</h4>
+            <TodoInput></TodoInput>
+            <div class="list-wrapper">
+              <ul
+                v-bind:key="todo.id"
+                v-for="todo in todos"
+                class="d-flex flex-column-reverse todo-list"
+              >
+                <TodoItem
+                  v-bind:todo="todo"
+                  v-on:del-todo="$emit('del-todo', todo.id)"
+                ></TodoItem>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoInput from "./TodoInput";
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "Todos",
+  components: {
+    TodoInput,
+    TodoItem
+  },
+  props: ["todos"]
+};
+</script>
+
+<style scoped>
+/* .todo-table{
+        height: 300px;
+        overflow-y: auto;
+        width: 50%;
+    } */
+</style>
